967ab81cf86704851d46b30e3e386874035a2211
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-10-26 23:55:12 +0000

    sql

--- Skilaverkefni_04/sql/Create_Tables.sql
+++ Skilaverkefni_04/sql/Create_Tables.sql
@@ -2,6 +2,7 @@ USE [hshjohannesh10]
 
 GO
 
+drop table [hshjohannesh10].[ru_driver_ratings]
 drop table [hshjohannesh10].[ru_trips]
 drop table [hshjohannesh10].[ru_drivers]
 drop table [hshjohannesh10].[ru_users]
@@ -38,6 +39,15 @@ CREATE TABLE ru_trips
   endTime bigint
 )
 
+CREATE TABLE ru_driver_ratings
+(
+  id int Identity (1, 1) primary key NOT NULL,
+  riderId int foreign key references ru_users(id) NOT NULL,
+  driverId int foreign key references ru_drivers(id) NOT NULL,
+  rating int NOT NULL,
+  comment nvarchar(500)
+ )
+
 GO
 
 INSERT INTO [hshjohannesh10].[ru_users]
@@ -47,15 +57,30 @@ INSERT INTO [hshjohannesh10].[ru_users]
            ,[password]
            ,[email]
            ,[registered])
-     VALUES
-           ('joezombie'
-           ,'Johannes'
-           ,'Heidarsson'
-           ,'joezombie'
-           ,'johannesh10@ru.is'
-           ,getdate())
+     SELECT 'joezombie', 'Johannes' ,'Heidarsson' ,'joezombie' ,'johannesh10@ru.is' ,getdate()
+	 UNION ALL
+	 SELECT 'tbickle', 'Travis' ,'Bickle' ,'tbickle' ,'travis@taxi.com' ,getdate()
 
 GO
+
+INSERT INTO [hshjohannesh10].[ru_drivers]
+           ([userId])
+     VALUES
+           (2)
+GO
+
+INSERT INTO [hshjohannesh10].[ru_driver_ratings]
+           ([riderId]
+           ,[driverId]
+           ,[rating]
+           ,[comment])
+     VALUES (1, 1, 5, 'The car was nice but the driver seemed a bit unstable')
+GO
+
+select * from [hshjohannesh10].[hshjohannesh10].[ru_users]
+select * from [hshjohannesh10].[hshjohannesh10].[ru_drivers]
+select * from [hshjohannesh10].[hshjohannesh10].[ru_trips]
+select * from [hshjohannesh10].[hshjohannesh10].[ru_driver_ratings]
 
 
 
